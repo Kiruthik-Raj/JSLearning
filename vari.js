9107ce03fe1642a348735c1b4f407075663c806e
Kiruthik-Raj <gmkiruthick02@gmail.com>
2024-08-10 09:34:11 +0530

    completed variables

---
+++ vari.js
@@ -0,0 +1,10 @@
+const account_no = 345825 // wont change at any cost, once declared
+let userEmail = "johnwick14@assassin.ace"
+var password = "JohnWick@2024"
+userCity = "New York"
+
+console.log(account_no)
+
+//tedious work to print variable data one by one, so use table method
+
+console.table([account_no, userEmail, password, userCity])

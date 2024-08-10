cc3c50f01202efa7dde9ffcbbb0698c4e868f597
Kiruthik-Raj <gmkiruthick02@gmail.com>
2024-08-10 09:51:42 +0530

    completed datatypes

---
+++ datatypes.js
@@ -0,0 +1,18 @@
+let myname = "Kiruthick"
+
+let age = 42
+
+let isPushedtoGit = true
+
+let num = null
+let rv;
+
+let symbol = "unique"
+
+console.log(typeof 32);
+
+console.log(typeof myname)
+
+console.log(typeof undefined)
+
+console.log(typeof null)

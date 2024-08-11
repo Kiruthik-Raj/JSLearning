896c1ab2be44f2d082805ec6c31e34dde8a7d700
Kiruthik-Raj <gmkiruthick02@gmail.com>
2024-08-11 07:35:27 +0530

    completed comparisons

---
+++ comparison.js
@@ -0,0 +1,12 @@
+//comparison of two datatypes or more using onditional statements
+
+//conditional statements include ==, != , <= , >= , >, <
+
+console.log(1  > "2")
+console.log("2" > 1)
+
+//null give false for greater than or equal to zero, but gives true for greater thean or equal to zero
+
+console.log("3" === 2)
+
+//=== - strict comparison (compares datatypes and the equality of the data)

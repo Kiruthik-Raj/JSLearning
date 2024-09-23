8bbafe57c8a332aff114fced48ca6d0a046acc74
Kiruthik-Raj <gmkiruthick02@gmail.com>
2024-09-23 22:27:04 +0530

    array methods

---
+++ arraymethods.js
@@ -0,0 +1,19 @@
+let arr = [1,2,3,4,5,6]
+
+console.log(arr)
+
+arr.pop()
+
+console.log(arr)
+
+arr.push(7)
+
+console.log(arr)
+
+arr.shift()
+
+console.log(arr)
+
+arr.unshift(0)
+
+console.log(arr)
